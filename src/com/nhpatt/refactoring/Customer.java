585e013bbb104615e3682e7559a3a45266604b5e
Javier Gamarra <nhpatt@gmail.com>
2011-07-31 14:14:01 +0200

    third step - move method and rename

--- src/com/nhpatt/refactoring/Customer.java
+++ src/com/nhpatt/refactoring/Customer.java
@@ -25,17 +25,17 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-		for (final Rental each : rentals) {
-			double thisAmount = amountFor(each);
+		for (final Rental rental : rentals) {
+			double thisAmount = rental.getCharge();
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
-					&& each.getDaysRented() > 1) {
+			if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+					&& rental.getDaysRented() > 1) {
 				frequentRenterPoints++;
 			}
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
+			result += "\t" + rental.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
@@ -43,29 +43,6 @@ public class Customer {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
-		return result;
-	}
-
-	private double amountFor(final Rental rental) {
-		double result = 0;
-		// determine amounts for each line
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (rental.getDaysRented() > 2) {
-				result += (rental.getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (rental.getDaysRented() > 3) {
-				result += (rental.getDaysRented() - 3) * 1.5;
-			}
-			break;
-		}
 		return result;
 	}
 
