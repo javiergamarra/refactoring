5bd1a0c87d11613516f88788ca4b4ee695255445
Javier Gamarra <nhpatt@gmail.com>
2011-07-31 14:09:12 +0200

    first step - switch to method

--- src/com/nhpatt/refactoring/Customer.java
+++ src/com/nhpatt/refactoring/Customer.java
@@ -26,25 +26,7 @@ public class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for (final Rental each : rentals) {
-			double thisAmount = 0;
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2) {
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3) {
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				}
-				break;
-			}
+			double thisAmount = amountFor(each);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -62,6 +44,29 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private double amountFor(final Rental each) {
+		double thisAmount = 0;
+		// determine amounts for each line
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2) {
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3) {
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			}
+			break;
+		}
+		return thisAmount;
 	}
 
 }
