134934f012ad32bcd8d4112485265eeedfcde887
Javier Gamarra <nhpatt@gmail.com>
2011-07-31 15:20:52 +0200

    fifth step - extract and move frequent renter points

--- src/com/nhpatt/refactoring/Customer.java
+++ src/com/nhpatt/refactoring/Customer.java
@@ -27,12 +27,7 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		for (final Rental rental : rentals) {
 			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-					&& rental.getDaysRented() > 1) {
-				frequentRenterPoints++;
-			}
+			frequentRenterPoints += rental.getFrequentRenterPoints();
 			// show figures for this rental
 			result += "\t" + rental.getMovie().getTitle() + "\t"
 					+ String.valueOf(rental.getCharge()) + "\n";
--- src/com/nhpatt/refactoring/Rental.java
+++ src/com/nhpatt/refactoring/Rental.java
@@ -41,4 +41,14 @@ public class Rental {
 		return result;
 	}
 
+	int getFrequentRenterPoints() {
+		int frequentRenterPoints = 1;
+		// add bonus for a two day new release rental
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE
+				&& getDaysRented() > 1) {
+			frequentRenterPoints++;
+		}
+		return frequentRenterPoints;
+	}
+
 }
